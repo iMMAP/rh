b1712f79f32773fd7ee354a9ca898e79683b3369
misagh <misagh.shakeri@gmail.com>
2022-07-24 16:27:21 +0200

    changing allowed hosts to dev

--- rh/settings.py
+++ rh/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4-vwuk3dtdi9jmc(&m4kqys%ltg_#rn0vt%z(=v2*9y68*@$9s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.120.128.159']
+ALLOWED_HOSTS = ['dev.reporthub.immap.org']
 
 
 # Application definition

d932e7e537246627151a6f02a93762896ef47835
obediobadiah <obediobadiah05@gmail.com>
2022-08-02 11:48:10 +0200

    fix comment

--- rh/views.py
+++ rh/views.py
@@ -22,27 +22,26 @@ def profile(request):
 
 
 def add_project(request):
-    if "Can view organization":
-        if request.method == 'POST':
-            form = ProjectForm(request.POST)
-            project = Project(
-            user=request.user,
-            title=request.POST['title'],
-            description=request.POST['description'],
-            start_date=request.POST.get('start_date'),
-            end_date=request.POST.get('end_date'),
-            )
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        project = Project(
+        user=request.user,
+        title=request.POST['title'],
+        description=request.POST['description'],
+        start_date=request.POST.get('start_date'),
+        end_date=request.POST.get('end_date'),
+        )
 
-            if form.is_valid():
-                project.save()
-                #return HttpResponseRedirect(reverse('index', args=(project.id,)))
-                # messages.info(request, "Report saved successfully")
-                return HttpResponseRedirect('/profile/')
+        if form.is_valid():
+            project.save()
+            #return HttpResponseRedirect(reverse('index', args=(project.id,)))
+            # messages.info(request, "Report saved successfully")
+            return HttpResponseRedirect('/profile/')
             
-        else:
-            form = ProjectForm()
-        return render(request, 'add_report.html', {'form': form})
-            # template = loader.get_template('report/add.html')
-            # user = request.user
-            # context = {'user': user}
-            # return HttpResponse(template.render(context, request))
+    else:
+        form = ProjectForm()
+    return render(request, 'add_report.html', {'form': form})
+        # template = loader.get_template('report/add.html')
+        # user = request.user
+        # context = {'user': user}
+        # return HttpResponse(template.render(context, request))

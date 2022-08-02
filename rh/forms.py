234c047f419f6a6053d6af61c4625377199ba5bb
obediobadiah <obediobadiah05@gmail.com>
2022-08-02 10:12:24 +0200

    feat_add_project

--- rh/forms.py
+++ rh/forms.py
@@ -1,7 +1,18 @@
 from django import forms
 
 class ProjectForm(forms.Form):
-    title = forms.CharField(label='Project Title', max_length=100)
-    description = forms.CharField(widget=forms.Textarea)
-    start_date = forms.DateField(widget=forms.widgets.SelectDateWidget)
-    end_date = forms.DateField(widget=forms.widgets.SelectDateWidget)
+    title = forms.CharField(widget=forms.TextInput(attrs={
+        "class":"w-full rounded-lg focus:ring-primary-600 focus:border-primary-600"
+    }), label='Project Title', max_length=100)
+    
+    description = forms.CharField(widget=forms.Textarea(attrs={
+        "class":"w-full rounded-lg focus:ring-primary-600 focus:border-primary-600"
+    }), label='Description')
+    start_date = forms.DateField(widget=forms.widgets.TextInput(attrs={
+        "type":"date",
+        "class": "focus:ring-primary-600 rounded-lg focus:border-primary-600 w-full"
+    }), label='Start date')
+    end_date = forms.DateField(widget=forms.widgets.TextInput(attrs={
+        "type":"date",
+        "class": " focus:ring-primary-600 rounded-lg focus:border-primary-600 w-full"
+    }), label='End date')

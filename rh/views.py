d6cb2f39b5aa8421a70868623da6824261e478d8
obediobadiah <obediobadiah05@gmail.com>
2022-08-02 10:12:24 +0200

    feat_add_project

--- rh/views.py
+++ rh/views.py
@@ -1,7 +1,9 @@
+from email import message
 from django.shortcuts import render
+from django.contrib import messages
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from .models import Project
 from .forms import ProjectForm
@@ -20,23 +22,27 @@ def profile(request):
 
 
 def add_project(request):
-    if request.method == 'POST':
-        project = Project(
+    if "Can view organization":
+        if request.method == 'POST':
+            form = ProjectForm(request.POST)
+            project = Project(
             user=request.user,
             title=request.POST['title'],
             description=request.POST['description'],
-            start_date=request.POST['start_date'],
-            end_date=request.POST['end_date'],
-        )
+            start_date=request.POST.get('start_date'),
+            end_date=request.POST.get('end_date'),
+            )
 
-        project.save()
-        #return HttpResponseRedirect(reverse('index', args=(project.id,)))
-        return HttpResponseRedirect('/profile/')
-    
-    else:
-        form = ProjectForm()
-        return render(request, 'report/add.html', {'form': form})
-        # template = loader.get_template('report/add.html')
-        # user = request.user
-        # context = {'user': user}
-        # return HttpResponse(template.render(context, request))
+            if form.is_valid():
+                project.save()
+                #return HttpResponseRedirect(reverse('index', args=(project.id,)))
+                # messages.info(request, "Report saved successfully")
+                return HttpResponseRedirect('/profile/')
+            
+        else:
+            form = ProjectForm()
+        return render(request, 'add_report.html', {'form': form})
+            # template = loader.get_template('report/add.html')
+            # user = request.user
+            # context = {'user': user}
+            # return HttpResponse(template.render(context, request))

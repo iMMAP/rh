1b4ef890ecf4a6d28192cbf00c700295d4b25b25
akhan <akhan@immap.org>
2022-09-07 11:19:32 +0500

    rh-aleem-signup[Sign up page progress]

--- rh/admin.py
+++ rh/admin.py
@@ -1,5 +1,7 @@
+from curses.ascii import US
 from django.contrib import admin
-from .models import Project
+from .models import Project, User
 
 
 admin.site.register(Project)
+admin.site.register(User)
--- rh/urls.py
+++ rh/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('logout/', views.logout_view, name='logout'),
+    path('register/', views.register_view, name='register'),
     path('profile/', views.profile, name='profile'),
     path('add_project/', views.add_project, name='add_project'),
 ]

5b017eb42dd2f10c2c1c1ed10c4600a292f06cbc
akhan <aleemcaan@gmail.com>
2022-09-11 08:29:11 +0500

    aleemcahn/PR10_FEAT/password_reset[Reformating files]

--- rh/forms.py
+++ rh/forms.py
@@ -2,8 +2,8 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, PasswordResetForm, SetPasswordForm, PasswordChangeForm
 from django.contrib.auth import get_user_model, password_validation
 
-
 User = get_user_model()
+
 
 class RegisterForm(UserCreationForm):
     """Subclass User Registration form"""
@@ -40,46 +40,47 @@ class UserPasswordChangeForm(PasswordChangeForm):
     def __init__(self, *args, **kwargs):
         """Call super"""
         super(UserPasswordChangeForm, self).__init__(*args, **kwargs)
-    
+
     old_password = forms.CharField(
         label='',
         strip=False,
         widget=forms.PasswordInput(
             attrs={
-                "autocomplete": "current-password", 
+                "autocomplete": "current-password",
                 "autofocus": True,
                 'class': 'form-control',
                 'placeholder': 'Old Password',
                 'type': 'password',
                 'name': 'old_password',
-                
+
             }
         ),
     )
 
-    new_password1 = forms.CharField(label='', 
-        help_text=password_validation.password_validators_help_text_html(),
-        widget=forms.PasswordInput(
-            attrs={
-                'class': 'form-control',
-                'placeholder': 'New Password',
-                'type': 'password',
-                'name': 'new_password1',
-                'autocomplete': 'new-password'
-            }
-        ),
-    )
-    new_password2 = forms.CharField(label='', 
-        widget=forms.PasswordInput(
-            attrs={
-                'class': 'form-control',
-                'placeholder': 'Confirm New Password',
-                'type': 'password',
-                'name': 'new_password2',
-                "autocomplete": "new-password"
-            }
-        ),
-    )
+    new_password1 = forms.CharField(label='',
+                                    help_text=password_validation.password_validators_help_text_html(),
+                                    widget=forms.PasswordInput(
+                                        attrs={
+                                            'class': 'form-control',
+                                            'placeholder': 'New Password',
+                                            'type': 'password',
+                                            'name': 'new_password1',
+                                            'autocomplete': 'new-password'
+                                        }
+                                    ),
+                                    )
+    new_password2 = forms.CharField(label='',
+                                    widget=forms.PasswordInput(
+                                        attrs={
+                                            'class': 'form-control',
+                                            'placeholder': 'Confirm New Password',
+                                            'type': 'password',
+                                            'name': 'new_password2',
+                                            "autocomplete": "new-password"
+                                        }
+                                    ),
+                                    )
+
 
 class UserSetPasswordForm(SetPasswordForm):
     """Override password reset form to add styling to the input fields"""
@@ -88,29 +89,29 @@ class UserSetPasswordForm(SetPasswordForm):
         """Call super"""
         super(UserSetPasswordForm, self).__init__(*args, **kwargs)
 
-    new_password1 = forms.CharField(label='', 
-        help_text=password_validation.password_validators_help_text_html(),
-        widget=forms.PasswordInput(
-            attrs={
-                'class': 'form-control',
-                'placeholder': 'New Password',
-                'type': 'password',
-                'name': 'new_password1',
-                'autocomplete': 'new-password'
-            }
-        ),
-    )
-    new_password2 = forms.CharField(label='', 
-        widget=forms.PasswordInput(
-            attrs={
-                'class': 'form-control',
-                'placeholder': 'Confirm New Password',
-                'type': 'password',
-                'name': 'new_password2',
-                "autocomplete": "new-password"
-            }
-        ),
-    )
+    new_password1 = forms.CharField(label='',
+                                    help_text=password_validation.password_validators_help_text_html(),
+                                    widget=forms.PasswordInput(
+                                        attrs={
+                                            'class': 'form-control',
+                                            'placeholder': 'New Password',
+                                            'type': 'password',
+                                            'name': 'new_password1',
+                                            'autocomplete': 'new-password'
+                                        }
+                                    ),
+                                    )
+    new_password2 = forms.CharField(label='',
+                                    widget=forms.PasswordInput(
+                                        attrs={
+                                            'class': 'form-control',
+                                            'placeholder': 'Confirm New Password',
+                                            'type': 'password',
+                                            'name': 'new_password2',
+                                            "autocomplete": "new-password"
+                                        }
+                                    ),
+                                    )
 
 
 class UserPasswordResetForm(PasswordResetForm):
@@ -120,31 +121,31 @@ class UserPasswordResetForm(PasswordResetForm):
         """Call super"""
         super(UserPasswordResetForm, self).__init__(*args, **kwargs)
 
-    email = forms.EmailField(label='', 
-        widget=forms.EmailInput(
-            attrs={
-                'class': 'form-control',
-                'placeholder': 'e.g abc@gmail.com',
-                'type': 'email',
-                'name': 'email'
-            }
-        ),
-    )
+    email = forms.EmailField(label='',
+                             widget=forms.EmailInput(
+                                 attrs={
+                                     'class': 'form-control',
+                                     'placeholder': 'e.g abc@gmail.com',
+                                     'type': 'email',
+                                     'name': 'email'
+                                 }
+                             ),
+                             )
 
 
 class ProjectForm(forms.Form):
     title = forms.CharField(widget=forms.TextInput(attrs={
-        "class":"w-full rounded-lg focus:ring-primary-600 focus:border-primary-600"
+        "class": "w-full rounded-lg focus:ring-primary-600 focus:border-primary-600"
     }), label='Project Title', max_length=100)
-    
+
     description = forms.CharField(widget=forms.Textarea(attrs={
-        "class":"w-full rounded-lg focus:ring-primary-600 focus:border-primary-600"
+        "class": "w-full rounded-lg focus:ring-primary-600 focus:border-primary-600"
     }), label='Description')
     start_date = forms.DateField(widget=forms.widgets.TextInput(attrs={
-        "type":"date",
+        "type": "date",
         "class": "focus:ring-primary-600 rounded-lg focus:border-primary-600 w-full"
     }), label='Start date')
     end_date = forms.DateField(widget=forms.widgets.TextInput(attrs={
-        "type":"date",
+        "type": "date",
         "class": " focus:ring-primary-600 rounded-lg focus:border-primary-600 w-full"
     }), label='End date')

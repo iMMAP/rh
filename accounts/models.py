04cc83e548879de27964cbf0b6a385aff53b9381
akhan <akhan@immap.org>
2023-03-17 10:14:58 +0500

    aleemcahn/PR32_updated_models[Model changes, Script changes, Migration data files updated]

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from rh.models import Organization, Cluster
+from rh.models import Organization, Cluster, Location
+from django.utils import timezone
 
 # Create your models here.
 
@@ -8,11 +9,25 @@ class Account(AbstractUser):
     """Inherit AbstractUser model and include our custom fields"""
     first_name = None
     last_name = None
-
+    
+    location = models.ForeignKey(Location, blank=True, null=True, on_delete=models.SET_NULL,)
     organization = models.ForeignKey(Organization, on_delete=models.SET_NULL, blank=True, null=True)
     name = models.CharField(max_length=200, blank=True, null=True)
     visits = models.IntegerField(blank=True, null=True)
     cluster = models.ForeignKey(Cluster, on_delete=models.SET_NULL, blank=True, null=True)
-    position = models.CharField(max_length=200, blank=True, null=True)
     phone = models.CharField(max_length=200, blank=True, null=True)
+    position = models.CharField(max_length=200, blank=True, null=True)
+    skype = models.CharField(max_length=200, blank=True, null=True)
+    old_id = models.CharField(max_length=200, blank=True, null=True)
+
     pass
+
+
+class LoginHistory(models.Model):
+    """Login History"""
+    user = models.ForeignKey(Account, on_delete=models.SET_NULL, blank=True, null=True)
+    last_logged_in = models.DateTimeField(blank=True, null=True, default=timezone.now)
+
+    def __str__(self):
+        return self.user.name
+    
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader

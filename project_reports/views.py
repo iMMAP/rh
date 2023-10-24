fc99c51170a913051f399eced1ca68aa4681a787
aleemcahn <akhan@immag.org>
2023-10-24 17:06:13 +0500

    [PROGRESS] - New development

--- project_reports/views.py
+++ project_reports/views.py
@@ -1,15 +1,10 @@
 
-import datetime
-import re
-import urllib
-
-from dateutil.relativedelta import relativedelta
-from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.forms import modelformset_factory
+from django.forms.models import inlineformset_factory
 from django.shortcuts import get_object_or_404, render
 from django.views.decorators.cache import cache_control
 
+from .forms import *
 from .models import *
 
 
@@ -48,14 +43,41 @@ def create_project_monthly_report_view(request, project):
     """Project Monthly Report Creation View"""
     project = get_object_or_404(Project, pk=project)
     project_state = project.state
+
+    # Get all existing activity plans for the project
+    activity_plans = project.activityplan_set.all()
+
+    # Create the activity plan formset with initial data from the project
+    ActivityReportFormset = inlineformset_factory(
+        ProjectMonthlyReport,
+        ActivityPlanReport,
+        form=ActivityPlanReportForm,
+        extra=len(activity_plans),
+        can_delete=True,
+    )
+
+    activity_report_formset = ActivityReportFormset(
+        request.POST or None,
+    )
+    # Loop through the forms in the formset and set initial values for specific fields
+    for i, form in enumerate(activity_report_formset.forms):
+        if i < len(activity_plans):
+            activity_plan = activity_plans[i]
+            form.initial = {'activity_plan': activity_plan}
+            form.fields['indicator'].queryset = activity_plan.indicators.all()
+
+
     parent_page = {
         'in-progress': 'active_projects',
         'draft': 'draft_projects',
         'done': 'completed_projects',
         'archive': 'archived_projects'
     }.get(project_state, None)
+
     context = {
         'project': project,
+        'activity_plans': activity_plans,
+        'activity_report_formset': activity_report_formset,
         'parent_page': parent_page,
         'project_view': False,
         'financial_view': False,

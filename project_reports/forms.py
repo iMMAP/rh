b0f4bda89df19b9275fbc46f1738c5980b537abe
aleemcahn <akhan@immag.org>
2023-10-24 17:06:13 +0500

    [PROGRESS] - New development

---
+++ project_reports/forms.py
@@ -0,0 +1,57 @@
+from django import forms
+from django.forms.models import inlineformset_factory
+from django.urls import reverse_lazy
+
+from .models import *
+
+# class TargetLocationForm(forms.ModelForm):
+#     class Meta:
+#         model = TargetLocation
+#         fields = "__all__"
+#         widgets = {
+#             'country': forms.widgets.HiddenInput(),
+#             'active': forms.widgets.HiddenInput(),
+#             'locations_group_by': forms.widgets.RadioSelect(),
+#             'district': forms.Select(
+#                 attrs={'locations-queries-url': reverse_lazy('ajax-load-locations')}),
+#             'zone': forms.Select(
+#                 attrs={'locations-queries-url': reverse_lazy('ajax-load-locations')}),
+#         }
+
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         self.fields['save'] = forms.BooleanField(required=False, initial=False,
+#                                                  widget=forms.HiddenInput(attrs={'name': self.prefix + '-save'}))
+#         self.fields['province'].queryset = self.fields['province'].queryset.filter(type='Province')
+#         self.fields['district'].queryset = self.fields['district'].queryset.filter(type='District')
+#         self.fields['zone'].queryset = self.fields['zone'].queryset.filter(type='Zone')
+#         self.fields['province'].widget.attrs.update({'data-form-prefix': f"{kwargs.get('prefix')}",
+#                                                      'onchange': f"updateLocationTitle('{kwargs.get('prefix')}', 'id_{kwargs.get('prefix')}-province');",
+#                                                      })
+#         self.fields['district'].widget.attrs.update(
+#             {'onchange': f"updateLocationTitle('{kwargs.get('prefix')}', 'id_{kwargs.get('prefix')}-district');",
+#              'locations-queries-url': reverse_lazy('ajax-load-locations')})
+#         self.fields['site_name'].widget.attrs.update(
+#             {'onchange': f"updateLocationTitle('{kwargs.get('prefix')}', 'id_{kwargs.get('prefix')}-site_name');"})
+
+
+# TargetLocationFormSet = inlineformset_factory(
+#         ActivityPlan,
+#         TargetLocation,
+#         form=TargetLocationForm,
+#         extra=0,  # Number of empty forms to display
+#         can_delete=True  # Allow deletion of existing forms
+#     )
+
+# DisaggregationFormSet = inlineformset_factory(
+#         TargetLocation,
+#         DisaggregationLocation,
+#         fields="__all__",
+#         extra=0,  # Number of empty forms to display
+#     )
+
+
+class ActivityPlanReportForm(forms.ModelForm):
+    class Meta:
+        model = ActivityPlanReport
+        fields = "__all__"

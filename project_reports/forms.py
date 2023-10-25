867a310339230458478a0d1d9a3088ce3fecc27a
aleemcahn <akhan@immag.org>
2023-10-25 14:57:18 +0500

    [PROGRESS] - Create Report Frontend Progress

--- project_reports/forms.py
+++ project_reports/forms.py
@@ -4,54 +4,71 @@ from django.urls import reverse_lazy
 
 from .models import *
 
-# class TargetLocationForm(forms.ModelForm):
-#     class Meta:
-#         model = TargetLocation
-#         fields = "__all__"
-#         widgets = {
-#             'country': forms.widgets.HiddenInput(),
-#             'active': forms.widgets.HiddenInput(),
-#             'locations_group_by': forms.widgets.RadioSelect(),
-#             'district': forms.Select(
-#                 attrs={'locations-queries-url': reverse_lazy('ajax-load-locations')}),
-#             'zone': forms.Select(
-#                 attrs={'locations-queries-url': reverse_lazy('ajax-load-locations')}),
-#         }
 
-#     def __init__(self, *args, **kwargs):
-#         super().__init__(*args, **kwargs)
-#         self.fields['save'] = forms.BooleanField(required=False, initial=False,
-#                                                  widget=forms.HiddenInput(attrs={'name': self.prefix + '-save'}))
-#         self.fields['province'].queryset = self.fields['province'].queryset.filter(type='Province')
-#         self.fields['district'].queryset = self.fields['district'].queryset.filter(type='District')
-#         self.fields['zone'].queryset = self.fields['zone'].queryset.filter(type='Zone')
-#         self.fields['province'].widget.attrs.update({'data-form-prefix': f"{kwargs.get('prefix')}",
-#                                                      'onchange': f"updateLocationTitle('{kwargs.get('prefix')}', 'id_{kwargs.get('prefix')}-province');",
-#                                                      })
-#         self.fields['district'].widget.attrs.update(
-#             {'onchange': f"updateLocationTitle('{kwargs.get('prefix')}', 'id_{kwargs.get('prefix')}-district');",
-#              'locations-queries-url': reverse_lazy('ajax-load-locations')})
-#         self.fields['site_name'].widget.attrs.update(
-#             {'onchange': f"updateLocationTitle('{kwargs.get('prefix')}', 'id_{kwargs.get('prefix')}-site_name');"})
+class ProjectMonthlyReportForm(forms.ModelForm):
+    class Meta:
+        model = ProjectMonthlyReport
+        fields = "__all__"
+
+        widgets = {
+            'report_date': forms.widgets.DateInput(
+                attrs={'type': 'date', 'onfocus': "(this.type='date')", 'onblur': "(this.type='text')"}),
+        }
 
 
-# TargetLocationFormSet = inlineformset_factory(
-#         ActivityPlan,
-#         TargetLocation,
-#         form=TargetLocationForm,
-#         extra=0,  # Number of empty forms to display
-#         can_delete=True  # Allow deletion of existing forms
-#     )
+class TargetLocationReportForm(forms.ModelForm):
+    class Meta:
+        model = TargetLocationReport
+        fields = "__all__"
+        # widgets = {
+        #     'country': forms.widgets.HiddenInput(),
+        #     'active': forms.widgets.HiddenInput(),
+        #     'locations_group_by': forms.widgets.RadioSelect(),
+        #     'district': forms.Select(
+        #         attrs={'locations-queries-url': reverse_lazy('ajax-load-locations')}),
+        #     'zone': forms.Select(
+        #         attrs={'locations-queries-url': reverse_lazy('ajax-load-locations')}),
+        # }
 
-# DisaggregationFormSet = inlineformset_factory(
-#         TargetLocation,
-#         DisaggregationLocation,
-#         fields="__all__",
-#         extra=0,  # Number of empty forms to display
-#     )
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     self.fields['save'] = forms.BooleanField(required=False, initial=False,
+    #                                              widget=forms.HiddenInput(attrs={'name': self.prefix + '-save'}))
+    #     self.fields['province'].queryset = self.fields['province'].queryset.filter(type='Province')
+    #     self.fields['district'].queryset = self.fields['district'].queryset.filter(type='District')
+    #     self.fields['zone'].queryset = self.fields['zone'].queryset.filter(type='Zone')
+    #     self.fields['province'].widget.attrs.update({'data-form-prefix': f"{kwargs.get('prefix')}",
+    #                                                  'onchange': f"updateLocationTitle('{kwargs.get('prefix')}', 'id_{kwargs.get('prefix')}-province');",
+    #                                                  })
+    #     self.fields['district'].widget.attrs.update(
+    #         {'onchange': f"updateLocationTitle('{kwargs.get('prefix')}', 'id_{kwargs.get('prefix')}-district');",
+    #          'locations-queries-url': reverse_lazy('ajax-load-locations')})
+    #     self.fields['site_name'].widget.attrs.update(
+    #         {'onchange': f"updateLocationTitle('{kwargs.get('prefix')}', 'id_{kwargs.get('prefix')}-site_name');"})
+
+
+TargetLocationReportFormSet = inlineformset_factory(
+        ActivityPlanReport,
+        TargetLocationReport,
+        form=TargetLocationReportForm,
+        extra=1,  # Number of empty forms to display
+        can_delete=True  # Allow deletion of existing forms
+    )
+
+DisaggregationReportFormSet = inlineformset_factory(
+        TargetLocationReport,
+        DisaggregationLocationReport,
+        fields="__all__",
+        extra=2,  # Number of empty forms to display
+    )
 
 
 class ActivityPlanReportForm(forms.ModelForm):
     class Meta:
         model = ActivityPlanReport
         fields = "__all__"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        self.fields['indicator'].widget.attrs.update({'class': 'report_indicator'})
--- project_reports/views.py
+++ project_reports/views.py
@@ -1,8 +1,11 @@
 
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from django.forms.models import inlineformset_factory
 from django.shortcuts import get_object_or_404, render
 from django.views.decorators.cache import cache_control
+
+from rh.models import *
 
 from .forms import *
 from .models import *
@@ -45,7 +48,23 @@ def create_project_monthly_report_view(request, project):
     project_state = project.state
 
     # Get all existing activity plans for the project
-    activity_plans = project.activityplan_set.all()
+    activity_plans = project.activityplan_set.select_related('activity_domain', 'activity_type', 'activity_detail').all()
+
+    # Get all existing target locaitions for the project
+    target_locations = project.targetlocation_set.select_related('province', 'district').all()
+    
+    # Create Q objects for each location type
+    province_q = Q(id__in=[location.province.id for location in target_locations if location.province])
+    district_q = Q(id__in=[location.district.id for location in target_locations if location.district])
+    zone_q = Q(id__in=[location.zone.id for location in target_locations if location.zone])
+
+    # Collect provinces, districts, and zones using a single query for each
+    target_location_provinces = Location.objects.filter(province_q)
+    target_location_districts = Location.objects.filter(district_q)
+    target_location_zones = Location.objects.filter(zone_q)
+
+    # Create the Project Monthly Report form
+    report_form = ProjectMonthlyReportForm(initial={'project': project})
 
     # Create the activity plan formset with initial data from the project
     ActivityReportFormset = inlineformset_factory(
@@ -59,11 +78,41 @@ def create_project_monthly_report_view(request, project):
     activity_report_formset = ActivityReportFormset(
         request.POST or None,
     )
-    # Loop through the forms in the formset and set initial values for specific fields
+
+    location_report_formsets = []
+    for activity_report in activity_report_formset.forms:
+        # Create a target location formset for each activity plan form
+        location_report_formset = TargetLocationReportFormSet(
+            request.POST or None,
+            instance=activity_report.instance,
+            prefix=f'locations_report_{activity_report.prefix}'
+        )
+        for location_report_form in location_report_formset.forms:
+            # Create a disaggregation formset for each target location form
+            disaggregation_report_formset = DisaggregationReportFormSet(
+                request.POST or None,
+                instance=location_report_form.instance,
+                prefix=f'disaggregation_report_{location_report_form.prefix}'
+            )
+            location_report_form.disaggregation_report_formset = disaggregation_report_formset
+
+
+        # Loop through the forms in the formset and set queryset values for specific fields
+        for i, form in enumerate(location_report_formset.forms):
+            if i < len(target_locations):
+                target_location = target_locations[i]
+                form.fields['province'].queryset = Location.objects.filter(id__in=target_location_provinces)
+                form.fields['district'].queryset = Location.objects.filter(id__in=target_location_districts)
+                form.fields['zone'].queryset = Location.objects.filter(id__in=target_location_zones)
+
+        location_report_formsets.append(location_report_formset)
+
+
+    # Loop through the forms in the formset and set initial and queryset values for specific fields
     for i, form in enumerate(activity_report_formset.forms):
         if i < len(activity_plans):
             activity_plan = activity_plans[i]
-            form.initial = {'activity_plan': activity_plan}
+            form.initial = {'activity_plan': activity_plan, 'project': project}
             form.fields['indicator'].queryset = activity_plan.indicators.all()
 
 
@@ -73,11 +122,16 @@ def create_project_monthly_report_view(request, project):
         'done': 'completed_projects',
         'archive': 'archived_projects'
     }.get(project_state, None)
+    
+    combined_formset = zip(activity_report_formset.forms, location_report_formsets)
 
     context = {
         'project': project,
         'activity_plans': activity_plans,
+        'report_form': report_form,
         'activity_report_formset': activity_report_formset,
+        'location_report_formset': location_report_formset,
+        'combined_formset': combined_formset,
         'parent_page': parent_page,
         'project_view': False,
         'financial_view': False,

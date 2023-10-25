7c6e1efc4237eb8ad8376224c5ea1df6e0686226
Shahryar Tayeb <shahryar.dev@gmail.com>
2023-10-25 11:50:57 +0430

    WIP - working on built files require error

--- core/settings/production.py
+++ core/settings/production.py
@@ -1,13 +1,32 @@
 from .base import *
 
 DATABASES = {
-    "default": {
+    "sqlite": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    },
+    "mysql": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": env("DB_NAME", default="test"),
+        "NAME": env("DB_NAME", default="rh"),
         "USER": env("DB_USER", default="root"),
         "PASSWORD": env("DB_PASSWORD", default=""),
         "HOST": env("DB_HOST", default="localhost"),
         "PORT": env("DB_PORT", default="3306"),
         "OPTIONS": {"charset": "utf8mb4"},
+    },
+}
+
+DB = env("DB", default="sqlite")
+
+DATABASES["default"] = DATABASES[DB]
+
+
+DJANGO_VITE_PLUGIN = {
+    "BUILD_DIR": "static-cdn/build",
+    "BUILD_URL_PREFIX": "/" + STATIC_URL + "build",
+    "DEV_MODE": False,
+    "STATIC_LOOKUP": False,
+    'JS_ATTRS': {
+        'type': 'module'
     },
 }

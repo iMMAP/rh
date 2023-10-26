eef4b33875f8ce785393154302cb5678f72d866b
Shahryar Tayeb <shahryar.dev@gmail.com>
2023-10-26 14:04:01 +0430

    WIP - Clean up

--- static/vite.config.js
+++ static/vite.config.js
@@ -1,15 +1,8 @@
 import { defineConfig } from "vite";
-// import inject from '@rollup/plugin-inject';
 import { djangoVitePlugin } from "django-vite-plugin";
 
 export default defineConfig({
   plugins: [
-    // inject({
-    //   // that should be first under plugins array
-    //   $: 'jquery',
-    //   jQuery: 'jquery',
-    // }),
-
     djangoVitePlugin({
       input: ["src/styles/style.scss", "src/js/app.js"],
       root: "..",

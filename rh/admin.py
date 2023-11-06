fc8381087a83f0bcdcf6357072c9afe8d80fe340
Shahryar Tayeb <shahryar.dev@gmail.com>
2023-11-06 11:14:00 +0430

    fixed linter and formatter issues and test runs

--- rh/admin.py
+++ rh/admin.py
@@ -3,7 +3,33 @@ from django.contrib import admin
 from django.contrib.admin.widgets import FilteredSelectMultiple
 from django.db.models import Count
 
-from .models import *
+from .models import (
+    Location,
+    Currency,
+    LocationType,
+    FacilitySiteType,
+    ImplementationModalityType,
+    TransferMechanismType,
+    PackageType,
+    TransferCategory,
+    GrantType,
+    UnitType,
+    ReportType,
+    ActivityPlan,
+    Cluster,
+    Disaggregation,
+    Organization,
+    BeneficiaryType,
+    Donor,
+    DisaggregationLocation,
+    Indicator,
+    ActivityDomain,
+    ActivityDetail,
+    ActivityType,
+    Project,
+    TargetLocation,
+    BudgetProgress,
+)
 
 admin.site.register(Currency)
 admin.site.register(LocationType)
@@ -16,35 +42,34 @@ admin.site.register(GrantType)
 admin.site.register(UnitType)
 admin.site.register(ReportType)
 
+
 class ActivityPlanModelAdminForm(forms.ModelForm):
     class Meta:
         model = ActivityPlan
-        fields = '__all__'
+        fields = "__all__"
         widgets = {
-            'indicators': FilteredSelectMultiple('Indicators', False),
+            "indicators": FilteredSelectMultiple("Indicators", False),
         }
+
 
 class DisseggregationModelAdminForm(forms.ModelForm):
     class Meta:
         model = Disaggregation
-        fields = '__all__'
+        fields = "__all__"
         widgets = {
-            'indicators': FilteredSelectMultiple('Indicators', False),
+            "indicators": FilteredSelectMultiple("Indicators", False),
         }
 
 
 class ClusterAdmin(admin.ModelAdmin):
-    list_display = ('name', 'code', 'title','countries_count','donors_count')
-    search_fields = ('code', 'name')
-    list_filter = ('code',)
+    list_display = ("name", "code", "title", "countries_count", "donors_count")
+    search_fields = ("code", "name")
+    list_filter = ("code",)
 
     # get the count of a many to many relationship with Donor model
     def get_queryset(self, request):
         queryset = super().get_queryset(request)
-        queryset = queryset.annotate(
-            donors_count=Count('donor'),
-            countries_count=Count('countries')
-        )
+        queryset = queryset.annotate(donors_count=Count("donor"), countries_count=Count("countries"))
         return queryset
 
     def donors_count(self, obj):
@@ -52,61 +77,78 @@ class ClusterAdmin(admin.ModelAdmin):
 
     def countries_count(self, obj):
         return obj.countries_count
-    
+
+
 admin.site.register(Cluster, ClusterAdmin)
 
 
 class LocationAdmin(admin.ModelAdmin):
-    list_display = ('name', 'parent', 'code', 'level', 'original_name', 'type')
-    list_filter = ('level', 'type')
-    search_fields = ('name', 'parent__name', 'level', 'type')
+    list_display = ("name", "parent", "code", "level", "original_name", "type")
+    list_filter = ("level", "type")
+    search_fields = ("name", "parent__name", "level", "type")
+
+
 admin.site.register(Location, LocationAdmin)
 
 
 class OrganizationAdmin(admin.ModelAdmin):
-    list_display = ('name', 'code', 'type', 'countries_count','clusters_count')
-    search_fields = ('name', 'type')
-    list_filter = ('type', )
+    list_display = ("name", "code", "type", "countries_count", "clusters_count")
+    search_fields = ("name", "type")
+    list_filter = ("type",)
 
     def countries_count(self, obj):
         return obj.countries.count()
-    
+
     def clusters_count(self, obj):
         return obj.clusters.count()
+
+
 admin.site.register(Organization, OrganizationAdmin)
 
 
 class DonorAdmin(admin.ModelAdmin):
-    list_display = ('name','countries_count','clusters_count')
-    search_fields = ('code', 'name',)
+    list_display = ("name", "countries_count", "clusters_count")
+    search_fields = (
+        "code",
+        "name",
+    )
     # list_filter = ('cluster', 'country')
-    
+
     def countries_count(self, obj):
         return obj.countries.count()
-    
+
     def clusters_count(self, obj):
         return obj.clusters.count()
-   
+
+
 admin.site.register(Donor, DonorAdmin)
 
 
 class BeneficiaryTypeAdmin(admin.ModelAdmin):
-    list_display = ('name', 'code', 'country')
-    search_fields = ('code', 'name', 'country__name')
-    list_filter = ('country',)
+    list_display = ("name", "code", "country")
+    search_fields = ("code", "name", "country__name")
+    list_filter = ("country",)
+
+
 admin.site.register(BeneficiaryType, BeneficiaryTypeAdmin)
 
+
 class DisaggregationAdmin(admin.ModelAdmin):
-    list_display = ('name','indicators_count','clusters_count')
-    search_fields = ('code', 'name',)
+    list_display = ("name", "indicators_count", "clusters_count")
+    search_fields = (
+        "code",
+        "name",
+    )
     form = DisseggregationModelAdminForm
     # list_filter = ('cluster', 'country')
-    
+
     def indicators_count(self, obj):
         return obj.indicators.count()
-    
+
     def clusters_count(self, obj):
         return obj.clusters.count()
+
+
 admin.site.register(Disaggregation, DisaggregationAdmin)
 
 admin.site.register(DisaggregationLocation)
@@ -116,34 +158,56 @@ class DisaggregationInline(admin.TabularInline):
     model = Disaggregation.indicators.through
     extra = 1
 
+
 class IndicatorAdmin(admin.ModelAdmin):
-    list_display = ('name', 'code',)
-    search_fields = ('activity_types__name', 'name', 'code')
+    list_display = (
+        "name",
+        "code",
+    )
+    search_fields = ("activity_types__name", "name", "code")
     # list_filter = ('activity_type',)
     inlines = [
         DisaggregationInline,
     ]
+
+
 admin.site.register(Indicator, IndicatorAdmin)
 
 
 class ActivityDomainAdmin(admin.ModelAdmin):
-    list_display = ('name', 'code', 'active')
-    search_fields = ('name', 'clusters__title', 'code', 'countries__name')
-    list_filter = ('clusters', 'countries')
+    list_display = ("name", "code", "active")
+    search_fields = ("name", "clusters__title", "code", "countries__name")
+    list_filter = ("clusters", "countries")
+
+
 admin.site.register(ActivityDomain, ActivityDomainAdmin)
 
 
 class ActivityTypeAdmin(admin.ModelAdmin):
-    list_display = ('name', 'code', 'activity_domain', 'active')
-    search_fields = ('name', 'activity_domain__name', 'clusters__title', 'code', 'countries__name')
-    list_filter = ('activity_domain', 'clusters', 'countries')
+    list_display = ("name", "code", "activity_domain", "active")
+    search_fields = (
+        "name",
+        "activity_domain__name",
+        "clusters__title",
+        "code",
+        "countries__name",
+    )
+    list_filter = ("activity_domain", "clusters", "countries")
+
+
 admin.site.register(ActivityType, ActivityTypeAdmin)
 
 
 class ActivityDetailAdmin(admin.ModelAdmin):
-    list_display = ('name', 'activity_type', 'code',)
-    search_fields = ('activity_type__name', 'name', 'code')
-    list_filter = ('activity_type',)
+    list_display = (
+        "name",
+        "activity_type",
+        "code",
+    )
+    search_fields = ("activity_type__name", "name", "code")
+    list_filter = ("activity_type",)
+
+
 admin.site.register(ActivityDetail, ActivityDetailAdmin)
 
 
@@ -151,33 +215,69 @@ admin.site.register(ActivityDetail, ActivityDetailAdmin)
 ####### Project Planning Model Admins #######
 #############################################
 
+
 class ProjectAdmin(admin.ModelAdmin):
-    list_display = ('title', 'code', 'user', 'show_clusters', 'budget', 'budget_currency', 'state', 'active')
+    list_display = (
+        "title",
+        "code",
+        "user",
+        "show_clusters",
+        "budget",
+        "budget_currency",
+        "state",
+        "active",
+    )
     search_fields = (
-        'title', 'code', 'clusters__title', 'implementing_partners__code',
-        'programme_partners__code',
-        'state')
-    list_filter = ('state', 'active', 'clusters')
+        "title",
+        "code",
+        "clusters__title",
+        "implementing_partners__code",
+        "programme_partners__code",
+        "state",
+    )
+    list_filter = ("state", "active", "clusters")
 
     def show_clusters(self, obj):
         return ",\n".join([a.title for a in obj.clusters.all()])
-    show_clusters.short_description = 'Clusters'
+
+    show_clusters.short_description = "Clusters"
+
+
 admin.site.register(Project, ProjectAdmin)
 
 
 class ActivityPlanAdmin(admin.ModelAdmin):
-    list_display = ('title', 'project', 'beneficiary', 'beneficiary_category', 'state', 'active')
-    search_fields = ('title', 'state', 'active', 'project__title')
-    list_filter = ('state', 'active', 'project__code')
+    list_display = (
+        "title",
+        "project",
+        "beneficiary",
+        "beneficiary_category",
+        "state",
+        "active",
+    )
+    search_fields = ("title", "state", "active", "project__title")
+    list_filter = ("state", "active", "project__code")
     form = ActivityPlanModelAdminForm
+
+
 admin.site.register(ActivityPlan, ActivityPlanAdmin)
 
 
-
 class TargetLocationAdmin(admin.ModelAdmin):
-    list_display = ('title', 'site_name', 'project', 'activity_plan', 'country', 'province', 'district', 'zone', 'state', 'active')
-    search_fields = ('title', 'project__title', 'state', 'active')
-    list_filter = ('state', 'active', 'project__code')
+    list_display = (
+        "title",
+        "site_name",
+        "project",
+        "activity_plan",
+        "country",
+        "province",
+        "district",
+        "zone",
+        "state",
+        "active",
+    )
+    search_fields = ("title", "project__title", "state", "active")
+    list_filter = ("state", "active", "project__code")
 
 
 admin.site.register(TargetLocation, TargetLocationAdmin)
@@ -187,10 +287,21 @@ admin.site.register(TargetLocation, TargetLocationAdmin)
 ######### Project Financial Report ###########
 ##############################################
 
+
 class BudgetProgressAdmin(admin.ModelAdmin):
     list_display = (
-        'project', 'donor', 'title', 'activity_domain', 'grant', 'amount_recieved', 'budget_currency', 'received_date', 'country')
-    search_fields = (
-        'project', 'donor', 'activity_domain', 'country')
-    list_filter = ('project', 'donor', 'country')
+        "project",
+        "donor",
+        "title",
+        "activity_domain",
+        "grant",
+        "amount_recieved",
+        "budget_currency",
+        "received_date",
+        "country",
+    )
+    search_fields = ("project", "donor", "activity_domain", "country")
+    list_filter = ("project", "donor", "country")
+
+
 admin.site.register(BudgetProgress, BudgetProgressAdmin)
--- rh/filters.py
+++ rh/filters.py
@@ -1,31 +1,31 @@
 import django_filters
 from django import forms
-from .models import *
+from .models import Cluster, Organization, Donor, ActivityDomain, Project
 
 
 class ProjectsFilter(django_filters.FilterSet):
     # FIXME: Fix the options issue
     clusters = django_filters.ModelMultipleChoiceFilter(
         queryset=Cluster.objects.all(),
-        widget=forms.SelectMultiple(attrs={'class': 'js_multiselect'}),
+        widget=forms.SelectMultiple(attrs={"class": "js_multiselect"}),
     )
     implementing_partners = django_filters.ModelMultipleChoiceFilter(
         queryset=Organization.objects.all(),
-        widget=forms.SelectMultiple(attrs={'class': 'js_multiselect'}),
+        widget=forms.SelectMultiple(attrs={"class": "js_multiselect"}),
     )
     donors = django_filters.ModelMultipleChoiceFilter(
         queryset=Donor.objects.all(),
-        widget=forms.SelectMultiple(attrs={'class': 'js_multiselect'}),
+        widget=forms.SelectMultiple(attrs={"class": "js_multiselect"}),
     )
     programme_partners = django_filters.ModelMultipleChoiceFilter(
         queryset=Organization.objects.all(),
-        widget=forms.SelectMultiple(attrs={'class': 'js_multiselect'}),
+        widget=forms.SelectMultiple(attrs={"class": "js_multiselect"}),
     )
     activity_domains = django_filters.ModelMultipleChoiceFilter(
         queryset=ActivityDomain.objects.all(),
-        widget=forms.SelectMultiple(attrs={'class': 'js_multiselect'}),
+        widget=forms.SelectMultiple(attrs={"class": "js_multiselect"}),
     )
 
     class Meta:
         model = Project
-        fields = '__all__'
+        fields = "__all__"

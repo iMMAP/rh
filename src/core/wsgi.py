29ef92e9287e4b4db5a16e4657bad040674b66dc
Shahryar Tayeb <shahryar.dev@gmail.com>
2023-11-13 11:32:56 +0430

    adjusted setting for dev server

--- src/core/wsgi.py
+++ src/core/wsgi.py
@@ -11,6 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+import environ
+
+environ.Env.read_env(os.path.join(os.path.dirname(os.path.dirname(__file__)), ".env"))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
 application = get_wsgi_application()

95293879fc13d51d064e38ec2aa34f82a89cdec4
Esmat-Farjad <esmatullah.hashimi2016@gmail.com>
2023-12-03 16:14:05 +0430

    project field extraction changes made

--- src/static/rh/js/all_projects.js
+++ src/static/rh/js/all_projects.js
@@ -10,6 +10,7 @@ $(function () {
 	});
     
 });
+
 const accHandler = document.querySelector(".project-accordion-handler");
 let arrowUp = document.querySelector(".accordion-arrow-up");
 let arrowDown = document.querySelector(".accordion-arrow-down");
@@ -21,6 +22,7 @@ accHandler.addEventListener("click", function(){
     if(arrowUp.classList.contains("hidden")){
         arrowDown.classList.add("hidden");
         arrowUp.classList.remove("hidden");
+        
     } else {
         arrowDown.classList.remove("hidden");
         arrowUp.classList.add("hidden");
@@ -32,11 +34,13 @@ let accarrowUp = document.querySelector(".activity-arrow-up");
 let accarrowDown = document.querySelector(".activity-arrow-down");
 accarrowUp.classList.add("hidden");
 activityAccordion.addEventListener("click", function(){
+
     let content = document.querySelector(".activity-accContainer");
     content.classList.toggle("hidden");
     if(accarrowUp.classList.contains("hidden")){
         accarrowDown.classList.add("hidden");
         accarrowUp.classList.remove("hidden");
+      
     } else {
         accarrowDown.classList.remove("hidden");
         accarrowUp.classList.add("hidden");

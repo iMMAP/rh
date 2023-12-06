89fa01d953c3dc0b09107a1e35e3e5b28a5b3e96
Shahryar Tayeb <shahryar.dev@gmail.com>
2023-12-06 16:16:39 +0430

    tested creation

--- src/rh/factory.py
+++ src/rh/factory.py
@@ -6,10 +6,18 @@ from .models import Disaggregation
 
 FAKE = faker.Faker()
 
+D_TYPES = [
+    ("public", "Public"),
+    ("private", "Private"),
+]
+
 class DisaggregationFactory(DjangoModelFactory):
     class Meta:
         model = Disaggregation
 
     name = factory.Faker("sentence", nb_words=1)
-    type = factory.Faker("slug")
+    type = factory.Iterator(D_TYPES, getter=lambda c: c[0])
+
+    # Run the bellow command in the shell to create fake data
+    # DisaggregationFactory.create_batch(amount)
     

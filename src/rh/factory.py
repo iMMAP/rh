7e0a3304a427c4ac485151f621abf39a264937ca
Shahryar Tayeb <shahryar.dev@gmail.com>
2023-12-10 15:39:45 +0430

    Done created factories for fake data for testing

--- src/rh/factory.py
+++ src/rh/factory.py
@@ -1,4 +1,6 @@
 import factory
+import random
+
 from factory.django import DjangoModelFactory
 from factory.faker import faker
 import pytz
@@ -16,8 +18,8 @@ from .models import (
     ActivityDetail,
     ActivityPlan,
     LocationType,
-TargetLocation,
-DisaggregationLocation
+    TargetLocation,
+    DisaggregationLocation,
 )
 from users.factory import UserFactory
 
@@ -40,18 +42,84 @@ class DisaggregationFactory(DjangoModelFactory):
     # DisaggregationFactory.create_batch(amount)
 
 
-class LocationFactory(DjangoModelFactory):
+class CountryFactory(DjangoModelFactory):
     class Meta:
         model = Location
 
-    name = factory.Faker("name")
-    level = factory.Faker("random_int", min=1, max=3)
-    original_name = factory.Faker("name")
-    type = factory.Iterator(Location.LOCATION_TYPES, getter=lambda c: c[0])
-    classification = factory.Iterator(Location.LOCATION_CLASSIFICATIONS, getter=lambda c: c[0])
+    name = factory.Faker("country")
+    level = 0
+    type = "Country"
+    classification = "urban"
     lat = factory.Faker("latitude")
     long = factory.Faker("longitude")
     parent = factory.LazyAttribute(lambda obj: None)
+
+    @factory.lazy_attribute
+    def original_name(self):
+        return self.name
+
+    @factory.lazy_attribute
+    def code(self):
+        return self.name.capitalize()
+
+
+class Admin1Factory(DjangoModelFactory):
+    class Meta:
+        model = Location
+
+    name = factory.Faker("city")
+    level = 1
+    type = "Province"
+    classification = factory.Iterator(Location.LOCATION_CLASSIFICATIONS, getter=lambda c: c[0])
+    lat = factory.Faker("latitude")
+    long = factory.Faker("longitude")
+    parent = factory.Iterator(Location.objects.filter(level=0))
+
+    @factory.lazy_attribute
+    def original_name(self):
+        return self.name
+
+    @factory.lazy_attribute
+    def code(self):
+        return self.name.capitalize()
+
+
+class Admin2Factory(DjangoModelFactory):
+    class Meta:
+        model = Location
+
+    name = factory.Faker("street_name")
+    level = 2
+    type = "District"
+    classification = factory.Iterator(Location.LOCATION_CLASSIFICATIONS, getter=lambda c: c[0])
+    lat = factory.Faker("latitude")
+    long = factory.Faker("longitude")
+    parent = factory.Iterator(Location.objects.filter(level=1))
+
+    @factory.lazy_attribute
+    def original_name(self):
+        return self.name
+
+    @factory.lazy_attribute
+    def code(self):
+        return self.name.capitalize()
+
+
+class Admin3Factory(DjangoModelFactory):
+    class Meta:
+        model = Location
+
+    name = factory.Faker("street_suffix")
+    level = 3
+    type = "Zone"
+    classification = factory.Iterator(Location.LOCATION_CLASSIFICATIONS, getter=lambda c: c[0])
+    lat = factory.Faker("latitude")
+    long = factory.Faker("longitude")
+    parent = factory.Iterator(Location.objects.filter(level=2))
+
+    @factory.lazy_attribute
+    def original_name(self):
+        return self.name
 
     @factory.lazy_attribute
     def code(self):
@@ -79,7 +147,7 @@ class ClusterFactory(factory.django.DjangoModelFactory):
             for location in extracted:
                 self.countries.add(location)
         else:
-            locations = Location.objects.filter(type="Country")
+            locations = Location.objects.filter(level=0)
 
             for location in locations:
                 self.countries.add(location)
@@ -91,7 +159,7 @@ class BeneficiaryTypeFactory(DjangoModelFactory):
 
     name = factory.Faker("word")
     code = factory.Faker("word")
-    country = factory.SubFactory(LocationFactory)
+    country = factory.SubFactory(CountryFactory)
     is_hrp_beneficiary = factory.Faker("boolean")
     is_regular_beneficiary = factory.Faker("boolean")
     start_date = factory.Faker("date_time_this_decade", tzinfo=pytz.UTC)
@@ -107,7 +175,7 @@ class BeneficiaryTypeFactory(DjangoModelFactory):
             for cluster in extracted:
                 self.clusters.add(cluster)
         else:
-            clusters = Location.objects.all()
+            clusters = Cluster.objects.all()
             for cluster in clusters:
                 self.clusters.add(cluster)
 
@@ -132,7 +200,9 @@ class OrganizationFactory(DjangoModelFactory):
             for country in extracted:
                 self.countries.add(country)
         else:
-            self.countries.add(LocationFactory())
+            countries = Location.objects.filter(level=0)
+            for country in countries:
+                self.countries.add(country)
 
     @factory.post_generation
     def clusters(self, create, extracted, **kwargs):
@@ -143,7 +213,9 @@ class OrganizationFactory(DjangoModelFactory):
             for cluster in extracted:
                 self.clusters.add(cluster)
         else:
-            self.clusters.add(ClusterFactory())
+            clusters = Cluster.objects.all()
+            for cluster in clusters:
+                self.clusters.add(cluster)
 
 
 class DonorFactory(DjangoModelFactory):
@@ -151,8 +223,32 @@ class DonorFactory(DjangoModelFactory):
         model = Donor
 
     name = factory.Faker("company")
-    countries = factory.RelatedFactory(LocationFactory)
-    clusters = factory.RelatedFactory(ClusterFactory)
+
+    @factory.post_generation
+    def countries(self, create, extracted, **kwargs):
+        if not create:
+            return
+
+        if extracted:
+            for country in extracted:
+                self.countries.add(country)
+        else:
+            countries = Location.objects.filter(level=0)
+            for country in countries:
+                self.countries.add(country)
+
+    @factory.post_generation
+    def clusters(self, create, extracted, **kwargs):
+        if not create:
+            return
+
+        if extracted:
+            for cluster in extracted:
+                self.clusters.add(cluster)
+        else:
+            clusters = Cluster.objects.all()
+            for cluster in clusters:
+                self.clusters.add(cluster)
 
     @factory.lazy_attribute
     def code(self):
@@ -166,34 +262,86 @@ class ActivityDomainFactory(DjangoModelFactory):
     active = factory.Faker("boolean")
     code = factory.Faker("pystr", max_chars=200)
     name = factory.Faker("word")
-    countries = factory.RelatedFactory(LocationFactory)
-    clusters = factory.RelatedFactory(ClusterFactory)
+
+    @factory.post_generation
+    def countries(self, create, extracted, **kwargs):
+        if not create:
+            return
+
+        if extracted:
+            for country in extracted:
+                self.countries.add(country)
+        else:
+            countries = Location.objects.filter(level=0)
+            for country in countries:
+                self.countries.add(country)
+
+    @factory.post_generation
+    def clusters(self, create, extracted, **kwargs):
+        if not create:
+            return
+
+        if extracted:
+            for cluster in extracted:
+                self.clusters.add(cluster)
+        else:
+            clusters = Cluster.objects.all()
+            for cluster in clusters:
+                self.clusters.add(cluster)
 
 
 class ActivityTypeFactory(DjangoModelFactory):
     class Meta:
         model = ActivityType
 
-    activity_domain = factory.SubFactory(ActivityDomainFactory)
+    activity_domain = factory.Iterator(ActivityDomain.objects.all())
+
     active = factory.Faker("boolean")
     code = factory.Faker("pystr", max_chars=600)
     name = factory.Faker("word")
-    countries = factory.RelatedFactory(LocationFactory)
-    clusters = factory.RelatedFactory(ClusterFactory)
+
     activity_date = factory.Faker("date_time")
     hrp_code = factory.Faker("pystr", max_chars=200)
     code_indicator = factory.Faker("boolean")
-    start_date = factory.Faker("date_time")
-    end_date = factory.Faker("date_time")
     ocha_code = factory.Faker("pystr", max_chars=200)
+
+    start_date = factory.Faker("date_time_this_decade", tzinfo=pytz.UTC)
+    end_date = factory.Faker("date_time_this_decade", tzinfo=pytz.UTC)
+
+    @factory.post_generation
+    def countries(self, create, extracted, **kwargs):
+        if not create:
+            return
+
+        if extracted:
+            for country in extracted:
+                self.countries.add(country)
+        else:
+            countries = Location.objects.filter(level=0)
+            for country in countries:
+                self.countries.add(country)
+
+    @factory.post_generation
+    def clusters(self, create, extracted, **kwargs):
+        if not create:
+            return
+
+        if extracted:
+            for cluster in extracted:
+                self.clusters.add(cluster)
+        else:
+            clusters = Cluster.objects.all()
+            for cluster in clusters:
+                self.clusters.add(cluster)
 
 
 class ActivityDetailFactory(DjangoModelFactory):
     class Meta:
         model = ActivityDetail
 
-    activity_type = factory.SubFactory(ActivityTypeFactory)
     name = factory.Faker("word")
+
+    activity_type = factory.Iterator(ActivityType.objects.all())
 
     @factory.lazy_attribute
     def code(self):
@@ -204,11 +352,24 @@ class IndicatorFactory(DjangoModelFactory):
     class Meta:
         model = Indicator
 
-    activity_types = factory.RelatedFactory(ActivityTypeFactory)
     name = factory.Faker("word")
     numerator = factory.Faker("pystr", max_chars=200)
     denominator = factory.Faker("pystr", max_chars=200)
     description = factory.Faker("text", max_nb_chars=1200)
+
+    @factory.post_generation
+    def activity_types(self, create, extracted, **kwargs):
+        if not create:
+            return
+
+        if extracted:
+            for activity_type in extracted:
+                self.activity_types.add(activity_type)
+        else:
+            activity_types = ActivityType.objects.all()
+            for activity_type in activity_types:
+                self.activity_types.add(activity_type)
+    
 
     @factory.lazy_attribute
     def code(self):
@@ -226,13 +387,15 @@ class ProjectFactory(DjangoModelFactory):
     is_hrp_project = factory.Faker("boolean")
     has_hrp_code = factory.Faker("boolean")
     hrp_code = factory.Faker("word")
-    clusters = factory.RelatedFactory(ClusterFactory)
-    activity_domains = factory.RelatedFactory(ActivityDomainFactory)
-    donors = factory.RelatedFactory(DonorFactory)
-    implementing_partners = factory.RelatedFactory(OrganizationFactory)
-    programme_partners = factory.RelatedFactory(OrganizationFactory)
-    start_date = factory.Faker("date_time")
-    end_date = factory.Faker("date_time")
+
+    # clusters = factory.RelatedFactory(ClusterFactory)
+    # activity_domains = factory.RelatedFactory(ActivityDomainFactory)
+    # donors = factory.RelatedFactory(DonorFactory)
+    # implementing_partners = factory.RelatedFactory(OrganizationFactory)
+    # programme_partners = factory.RelatedFactory(OrganizationFactory)
+
+    start_date = factory.Faker("date_time_this_decade", tzinfo=pytz.UTC)
+    end_date = factory.Faker("date_time_this_decade", tzinfo=pytz.UTC)
     budget = factory.Faker("random_int")
     budget_received = factory.Faker("random_int")
     budget_gap = factory.Faker("random_int")
@@ -241,6 +404,82 @@ class ProjectFactory(DjangoModelFactory):
     description = factory.Faker("paragraph")
     old_id = factory.Faker("word")
 
+    @factory.post_generation
+    def activity_plans(self, create, extracted, **kwargs):
+        if not create:
+            return
+
+        if extracted:
+            for activity_plan in extracted:
+                self.activityplan_set.add(activity_plan)
+        else:
+            for _ in range(random.randint(1, 7)):
+                self.activityplan_set.add(ActivityPlanFactory(project=self))
+
+    @factory.post_generation
+    def clusters(self, create, extracted, **kwargs):
+        if not create:
+            return
+
+        if extracted:
+            for cluster in extracted:
+                self.clusters.add(cluster)
+        else:
+            clusters = Cluster.objects.all()[: random.randint(1, 5)]
+            for cluster in clusters:
+                self.clusters.add(cluster)
+
+    @factory.post_generation
+    def activity_domains(self, create, extracted, **kwargs):
+        if not create:
+            return
+
+        if extracted:
+            for activity_domain in extracted:
+                self.activity_domains.add(activity_domain)
+        else:
+            activity_domains = ActivityDomain.objects.all()[: random.randint(1, 5)]
+            for ad in activity_domains:
+                self.activity_domains.add(ad)
+
+    @factory.post_generation
+    def donors(self, create, extracted, **kwargs):
+        if not create:
+            return
+
+        if extracted:
+            for donor in extracted:
+                self.donors.add(donor)
+        else:
+            donors = Donor.objects.all()[: random.randint(1, 5)]
+            for d in donors:
+                self.donors.add(d)
+
+    @factory.post_generation
+    def implementing_partners(self, create, extracted, **kwargs):
+        if not create:
+            return
+
+        if extracted:
+            for partner in extracted:
+                self.implementing_partners.add(partner)
+        else:
+            orgs = Organization.objects.all()[: random.randint(1, 5)]
+            for org in orgs:
+                self.implementing_partners.add(org)
+
+    @factory.post_generation
+    def programme_partners(self, create, extracted, **kwargs):
+        if not create:
+            return
+
+        if extracted:
+            for partner in extracted:
+                self.programme_partners.add(partner)
+        else:
+            orgs = Organization.objects.all()[: random.randint(1, 5)]
+            for org in orgs:
+                self.programme_partners.add(org)
 
 
 class ActivityPlanFactory(DjangoModelFactory):
@@ -248,45 +487,59 @@ class ActivityPlanFactory(DjangoModelFactory):
         model = ActivityPlan
 
     project = factory.SubFactory(ProjectFactory)
-    title = factory.Faker('sentence', nb_words=4)
-    activity_domain = factory.SubFactory(ActivityDomainFactory)
-    activity_type = factory.SubFactory(ActivityTypeFactory)
-    activity_detail = factory.SubFactory(ActivityDetailFactory)
-    beneficiary = factory.SubFactory(BeneficiaryTypeFactory)
-    hrp_beneficiary = factory.SubFactory(BeneficiaryTypeFactory)
+
+    title = factory.Faker("sentence", nb_words=4)
+
+    activity_domain = factory.Iterator(ActivityDomain.objects.all())
+
+    activity_type = factory.Iterator(ActivityType.objects.all())
+
+    activity_detail = factory.Iterator(ActivityDetail.objects.all())
+
+    beneficiary = factory.Iterator(BeneficiaryType.objects.all())
+
+    hrp_beneficiary = factory.Iterator(BeneficiaryType.objects.filter(is_hrp_beneficiary=True))
     # Add other fields if necessary
-    
-    
-    
+
+
 class LocationTypeFactory(DjangoModelFactory):
     class Meta:
         model = LocationType
 
-    name = factory.Faker('word')
-    
+    name = factory.Faker("word")
+
+
 class TargetLocationFactory(DjangoModelFactory):
     class Meta:
         model = TargetLocation
 
     project = factory.SubFactory(ProjectFactory)
-    activity_plan = factory.SubFactory(ActivityPlanFactory)
-    title = factory.Faker('sentence', nb_words=4)
-    country = factory.SubFactory(LocationFactory)
-    province = factory.SubFactory(LocationFactory)
-    district = factory.SubFactory(LocationFactory)
-    zone = factory.SubFactory(LocationFactory)
+
+    # activity_plan = factory.SubFactory(ActivityPlanFactory)
+    activity_plan = factory.SubFactory(ActivityPlanFactory, project=factory.SelfAttribute('..project'))
+
+    title = factory.Faker("sentence", nb_words=4)
+
+    country = factory.Iterator(Location.objects.filter(level=0))
+    province = factory.Iterator(Location.objects.filter(level=1))
+    district = factory.Iterator(Location.objects.filter(level=2))
+    zone = factory.Iterator(Location.objects.filter(level=3))
+
     location_type = factory.SubFactory(LocationTypeFactory)
-    implementing_partner = factory.SubFactory(OrganizationFactory)
-    site_name = factory.Faker('word')
-    site_lat = factory.Faker('latitude')
-    site_long = factory.Faker('longitude')
+
+    implementing_partner = factory.Iterator(Organization.objects.all())
+
+    site_name = factory.Faker("word")
+    site_lat = factory.Faker("latitude")
+    site_long = factory.Faker("longitude")
     # Add other fields if necessary
-    
+
+
 class DisaggregationLocationFactory(DjangoModelFactory):
     class Meta:
         model = DisaggregationLocation
 
-    active = factory.Faker('boolean')
+    active = factory.Faker("boolean")
     target_location = factory.SubFactory(TargetLocationFactory)
     disaggregation = factory.SubFactory(DisaggregationFactory)
-    target = factory.Faker('random_int')
+    target = factory.Faker("random_int")
--- src/rh/management/commands/seed.py
+++ src/rh/management/commands/seed.py
@@ -1,7 +1,27 @@
 from django.core.management.base import BaseCommand
 
 # from users.factory import UserFactory
-from rh.factory import DisaggregationFactory,ProjectFactory
+from rh.factory import (
+    ClusterFactory,
+    OrganizationFactory,
+    DonorFactory,
+    CountryFactory,
+    Admin1Factory,
+    Admin2Factory,
+    Admin3Factory,
+    BeneficiaryTypeFactory,
+    ActivityDomainFactory,
+    ActivityTypeFactory,
+    ActivityDetailFactory,
+    DisaggregationFactory,
+    ProjectFactory,
+    IndicatorFactory
+
+)
+
+# from users.factory import ProfileFactory
+from django.contrib.auth.models import User
+# from django.contrib.auth.hashers import make_password
 
 
 class Command(BaseCommand):
@@ -14,8 +34,30 @@ class Command(BaseCommand):
         # parser.add_argument('amount', nargs='+', type=int)
 
     def _generate_data(self, amount):
+        password = "bcrypt$$2b$12$eRuG.5HPTy3ZaFrQmFMox.3Zvf0p5pj8Z0qYDh/j1Cetr93Jm005a"  # admin
+        User.objects.update_or_create(username="admin", email="admin@admin.com", password=password, is_superuser=1)
+
+        # Fake Locations
+        CountryFactory.create_batch(amount)
+        Admin1Factory.create_batch(amount * 2)
+        Admin2Factory.create_batch(amount * 2)
+        Admin3Factory.create_batch(amount * 2)
+
+        # Fake main activities
+        ClusterFactory.create_batch(amount)
+        OrganizationFactory.create_batch(amount)
+        DonorFactory.create_batch(amount)
+        BeneficiaryTypeFactory.create_batch(amount)
+        ActivityDomainFactory.create_batch(amount)
+        ActivityTypeFactory.create_batch(amount)
+        ActivityDetailFactory.create_batch(amount)
         DisaggregationFactory.create_batch(amount)
+        IndicatorFactory.create_batch(amount)
+        # ProfileFactory.create_batch(amount)
+
+        # Fake projects
         ProjectFactory.create_batch(amount)
+
 
     def handle(self, *args, **options):
         amount = options.get("amount") or 10

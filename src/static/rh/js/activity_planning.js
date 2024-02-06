070bef79ed8f7156edd58d341f3ce2ac307c45a0
aleemcahn <akhan@immap.org>
2024-02-06 13:21:01 +0500

    [ADDED] - Facility/Sites Monitoring

--- src/static/rh/js/activity_planning.js
+++ src/static/rh/js/activity_planning.js
@@ -455,6 +455,36 @@ function getLocations(
 	}
 }
 
+
+/**
+* Handle Facility Monitoring field
+@param {string} locationPrefix - Form Location Prefix.
+@param {string} formElement - Form Element.
+**/
+function handleFacilityMonitoring(locationPrefix, formElement) {
+	debugger
+	// id_target_locations_activityplan_set-0-0-facility_monitoring
+	// id_target_locations_activityplan_set-0-0-facility_monitoring
+	// id_target_locations_activityplan_set-0-0-facility_monitoring
+	$formElement = $(formElement)
+	let $facilityMonitoring = $(`#id_${locationPrefix}-facility_monitoring`);
+	let $facilityName = $formElement.find(
+		`#id_${locationPrefix}-facility_name`
+	);
+	let $facilityId = $formElement.find(`#id_${locationPrefix}-facility_id`);
+	let $facilityDetails = $formElement.find(
+		`#facility_details_${locationPrefix}`
+	);
+
+	if (!$facilityMonitoring.is(":checked")) {
+		$facilityDetails.hide();
+		$facilityName.prop("required", false).removeClass("is-required");
+	} else {
+		$facilityDetails.show();
+		$facilityName.prop("required", true).addClass("is-required");
+	}
+}
+
 /**
  * Ready Function
  **/
@@ -515,6 +545,7 @@ $(function () {
 			let selectedID = $(indicatorsSelect)[0].value
 			handleDisaggregationForms(indicatorsSelect, selectedID);
 		});
+
 	});
 
 	const $locationBlock = $(".target_location_form");
@@ -534,6 +565,18 @@ $(function () {
 		});
 		$(`#id_${locationPrefix}-district`).on("change", function () {
 			getLocations(locationPrefix, "zone", "district");
+		});
+
+		// Call Facility Monitoring function when page loads.
+		handleFacilityMonitoring(locationPrefix, formElement);
+		
+		// var $facilityMonitoring = $(`id_${locationPrefix}-facility_monitoring`)
+		debugger
+		let $facilityMonitoring = $(formElement).find(
+			`#id_${locationPrefix}-facility_monitoring`
+		);
+		$facilityMonitoring.change(function () {
+			handleFacilityMonitoring(locationPrefix, formElement);
 		});
 	});
 });

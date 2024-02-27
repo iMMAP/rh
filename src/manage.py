b3ab0458079e77f68f9ee50c152ee3789e387514
Shahryar Tayeb <shahryar.dev@gmail.com>
2024-02-27 11:49:20 +0430

    custom error pages

--- src/manage.py
+++ src/manage.py
@@ -7,8 +7,6 @@ import environ
 
 def main():
     """Run administrative tasks."""
-    # env = environ.Env()
-    environ.Env.read_env(os.path.join(os.path.dirname(os.path.dirname(__file__)), ".env"))
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.local")
 
     try:

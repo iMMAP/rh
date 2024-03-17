83250dd0398b96a67d9739b664ac327112543bb5
aleemcahn <aleemcaan@gmail.com>
2024-03-17 16:08:44 +0500

    [Progress] - filters and paginator added

---
+++ src/project_reports/filters.py
@@ -0,0 +1,23 @@
+import django_filters
+from django import forms
+
+from rh.models import Cluster, Organization
+from .models import ProjectMonthlyReport
+
+
+class ReportFilterForm(django_filters.FilterSet):
+    clusters = django_filters.ModelMultipleChoiceFilter(
+        # field_name='test',
+        # to_field_name='project',
+        queryset=Cluster.objects.all(),
+        widget=forms.SelectMultiple(),
+    )
+    organization = django_filters.ModelMultipleChoiceFilter(
+        queryset=Organization.objects.all(),
+        widget=forms.SelectMultiple(),
+    )
+    
+
+    class Meta:
+        model = ProjectMonthlyReport
+        fields = "__all__"

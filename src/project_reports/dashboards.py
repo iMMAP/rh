105ea63bfc9ecd47deaea9b7c4a2ce49b49486b5
aleemcahn <akhan@immap.org>
2024-03-17 16:49:54 +0500

    [Progress] - Style and links fixed

--- src/project_reports/dashboards.py
+++ src/project_reports/dashboards.py
@@ -1,6 +1,5 @@
 from django.core.paginator import Paginator
-from django.shortcuts import get_object_or_404, redirect, render
-from rh.models import Project
+from django.shortcuts import render
 
 from .filters import ReportFilterForm
 from .models import ProjectMonthlyReport
@@ -13,8 +12,7 @@ def reports_dashboard_view(request):
     # Setup Filter
     reports_filter = ReportFilterForm(
         request.GET,
-        queryset=ProjectMonthlyReport.objects.all()
-        .order_by("-id"),
+        queryset=ProjectMonthlyReport.objects.all().order_by("-id"),
     )
 
     # Setup Pagination

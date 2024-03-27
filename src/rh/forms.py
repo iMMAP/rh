25b6ff81fcc988000eda615c17df969482859750
aleemcahn <akhan@immap.org>
2024-03-27 09:58:37 +0500

    [Minor Changes]

--- src/rh/forms.py
+++ src/rh/forms.py
@@ -23,11 +23,7 @@ class ProjectForm(forms.ModelForm):
         fields = "__all__"
 
         widgets = {
-            "budget":forms.NumberInput(
-                attrs={
-                    "max":20000000
-                }
-            ),
+            "budget": forms.NumberInput(attrs={"max": 20000000}),
             "clusters": forms.SelectMultiple(
                 attrs={
                     "class": "custom-select",

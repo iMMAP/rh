659ffbbbfd85c0da39151a03997fcb353cac6428
Shahryar Tayeb <shahryar.dev@gmail.com>
2024-05-30 15:59:44 +0430

    js linter fixes

--- src/static/src/js/plugins/tabsPlugin.js
+++ src/static/src/js/plugins/tabsPlugin.js
@@ -3,8 +3,7 @@
  * jQuery Tabs plugin
  */
 
-; (function ($, $win) {
-  'use strict';
+; ((($, $win) => {
 
   function Tabset ($holder, options) {
     this.$holder = $holder;
@@ -28,44 +27,43 @@
     },
 
     setStartActiveIndex: function () {
-      var $classTargets = this.getClassTarget(this.$tabLinks);
-      var $activeLink = $classTargets.filter('.' + this.options.activeClass);
-      var $hashLink = this.$tabLinks.filter('[' + this.options.attrib + '="' + location.hash + '"]');
-      var activeIndex;
+      const $classTargets = this.getClassTarget(this.$tabLinks);
+      let $activeLink = $classTargets.filter(`.${this.options.activeClass}`);
+      const $hashLink = this.$tabLinks.filter(`[${this.options.attrib}="${location.hash}"]`);
+      
 
       if (this.options.checkHash && $hashLink.length) {
         $activeLink = $hashLink;
       }
 
-      activeIndex = $classTargets.index($activeLink);
+     const activeIndex = $classTargets.index($activeLink);
 
       this.activeTabIndex = this.prevTabIndex = (activeIndex === -1 ? (this.options.defaultTab ? 0 : null) : activeIndex);
     },
 
     setActiveTab: function () {
-      var self = this;
 
-      this.$tabLinks.each(function (i, link) {
-        var $link = $(link);
-        var $classTarget = self.getClassTarget($link);
-        var $tab = $($link.attr(self.options.attrib));
+      this.$tabLinks.each((i, link) => {
+        const $link = $(link);
+        const $classTarget = this.getClassTarget($link);
+        const $tab = $($link.attr(this.options.attrib));
 
-        if (i !== self.activeTabIndex) {
-          $classTarget.removeClass(self.options.activeClass);
-          $tab.addClass(self.options.tabHiddenClass).removeClass(self.options.activeClass);
+        if (i !== this.activeTabIndex) {
+          $classTarget.removeClass(this.options.activeClass);
+          $tab.addClass(this.options.tabHiddenClass).removeClass(this.options.activeClass);
         } else {
-          $classTarget.addClass(self.options.activeClass);
-          $tab.removeClass(self.options.tabHiddenClass).addClass(self.options.activeClass);
+          $classTarget.addClass(this.options.activeClass);
+          $tab.removeClass(this.options.tabHiddenClass).addClass(this.options.activeClass);
         }
 
-        self.attachTabLink($link, i);
+        this.attachTabLink($link, i);
       });
     },
 
     attachTabLink: function ($link, i) {
-      var self = this;
+      const self = this;
 
-      $link.on(this.options.event + '.tabset', function (e) {
+      $link.on(`${this.options.event}.tabset`, function (e) {
         e.preventDefault();
 
         if (self.activeTabIndex === self.prevTabIndex && self.activeTabIndex !== i) {
@@ -79,51 +77,49 @@
     },
 
     resizeHolder: function (height) {
-      var self = this;
 
       if (height) {
         this.$tabHolder.height(height);
-        setTimeout(function () {
-          self.$tabHolder.addClass('transition');
+        setTimeout(() => {
+          this.$tabHolder.addClass('transition');
         }, 10);
       } else {
-        self.$tabHolder.removeClass('transition').height('');
+        this.$tabHolder.removeClass('transition').height('');
       }
     },
 
     switchTabs: function () {
-      var self = this;
 
-      var $prevLink = this.$tabLinks.eq(this.prevTabIndex);
-      var $nextLink = this.$tabLinks.eq(this.activeTabIndex);
+      const $prevLink = this.$tabLinks.eq(this.prevTabIndex);
+      const $nextLink = this.$tabLinks.eq(this.activeTabIndex);
 
-      var $prevTab = this.getTab($prevLink);
-      var $nextTab = this.getTab($nextLink);
+      const $prevTab = this.getTab($prevLink);
+      const $nextTab = this.getTab($nextLink);
 
       $prevTab.removeClass(this.options.activeClass);
 
-      if (self.haveTabHolder()) {
+      if (this.haveTabHolder()) {
         this.resizeHolder($prevTab.outerHeight());
       }
 
-      setTimeout(function () {
-        self.getClassTarget($prevLink).removeClass(self.options.activeClass);
+      setTimeout(() => {
+        this.getClassTarget($prevLink).removeClass(this.options.activeClass);
 
-        $prevTab.addClass(self.options.tabHiddenClass);
-        $nextTab.removeClass(self.options.tabHiddenClass).addClass(self.options.activeClass);
+        $prevTab.addClass(this.options.tabHiddenClass);
+        $nextTab.removeClass(this.options.tabHiddenClass).addClass(this.options.activeClass);
 
-        self.getClassTarget($nextLink).addClass(self.options.activeClass);
+        this.getClassTarget($nextLink).addClass(this.options.activeClass);
 
-        if (self.haveTabHolder()) {
-          self.resizeHolder($nextTab.outerHeight());
+        if (this.haveTabHolder()) {
+          this.resizeHolder($nextTab.outerHeight());
 
-          setTimeout(function () {
-            self.resizeHolder();
-            self.prevTabIndex = self.activeTabIndex;
-            self.makeCallback('onChange', self);
-          }, self.options.animSpeed);
+          setTimeout(() => {
+            this.resizeHolder();
+            this.prevTabIndex = this.activeTabIndex;
+            this.makeCallback('onChange', this);
+          }, this.options.animSpeed);
         } else {
-          self.prevTabIndex = self.activeTabIndex;
+          this.prevTabIndex = this.activeTabIndex;
         }
       }, this.options.autoHeight ? this.options.animSpeed : 1);
     },
@@ -141,17 +137,17 @@
     },
 
     haveTabHolder: function () {
-      return this.$tabHolder && this.$tabHolder.length;
+      return this.$tabHolder?.length;
     },
 
     destroy: function () {
-      var self = this;
+      const self = this;
 
       this.$tabLinks.off('.tabset').each(function () {
-        var $link = $(this);
+        const $link = $(this);
 
         self.getClassTarget($link).removeClass(self.options.activeClass);
-        $($link.attr(self.options.attrib)).removeClass(self.options.activeClass + ' ' + self.options.tabHiddenClass);
+        $($link.attr(self.options.attrib)).removeClass(`${self.options.activeClass} ${self.options.tabHiddenClass}`);
       });
 
       this.$holder.removeData('Tabset');
@@ -159,7 +155,8 @@
 
     makeCallback: function (name) {
       if (typeof this.options[name] === 'function') {
-        var args = Array.prototype.slice.call(arguments);
+        // biome-ignore lint/style/noArguments: <explanation>
+        const args = Array.prototype.slice.call(arguments);
         args.shift();
         this.options[name].apply(this, args);
       }
@@ -167,10 +164,11 @@
   };
 
   $.fn.tabset = function (opt) {
-    var args = Array.prototype.slice.call(arguments);
-    var method = args[0];
+    // biome-ignore lint/style/noArguments: <explanation>
+    const args = Array.prototype.slice.call(arguments);
+    const method = args[0];
 
-    var options = $.extend({
+    const options = $.extend({
       activeClass: 'active',
       addToParent: false,
       autoHeight: false,
@@ -182,11 +180,12 @@
       event: 'click',
       tabHiddenClass: 'js-tab-hidden'
     }, opt);
+    // biome-ignore lint/correctness/noSelfAssign: <explanation>
     options.autoHeight = options.autoHeight;
 
     return this.each(function () {
-      var $holder = jQuery(this);
-      var instance = $holder.data('Tabset');
+      const $holder = jQuery(this);
+      const instance = $holder.data('Tabset');
 
       if (typeof opt === 'object' || typeof opt === 'undefined') {
         $holder.data('Tabset', new Tabset($holder, options));
@@ -198,4 +197,4 @@
       }
     });
   };
-}(jQuery, jQuery(window)));
+})(jQuery, jQuery(window)));

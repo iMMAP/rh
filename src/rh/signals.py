154b2e92d290dfe7b52b683782d331520c991186
Shahryar Tayeb <shahryar.dev@gmail.com>
2024-06-06 10:43:44 +0430

    add admin groups after creation of an organization or cluster

---
+++ src/rh/signals.py
@@ -0,0 +1,23 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import Group
+from .models import Cluster,Organization
+from users.utils import assign_default_permissions_to_group
+
+
+@receiver(post_save, sender=Cluster)
+def post_create_cluster(sender, instance, created, **kwargs):
+    if created:
+        group, created = Group.objects.get_or_create(name=f"{instance.code.upper()}_CLUSTER_LEADS")
+        if created:
+            # Assign default permissions to the group
+            assign_default_permissions_to_group(source_group_name="CLUSTER_LEAD",target_group=group)
+
+
+@receiver(post_save, sender=Organization)
+def post_create_org(sender, instance, created, **kwargs):
+    if created:
+        group, created = Group.objects.get_or_create(name=f"{instance.code.upper()}_ORG_LEAD")
+        if created:
+            # Assign default permissions to the group
+            assign_default_permissions_to_group(source_group_name="ORG_LEAD", target_group=group)

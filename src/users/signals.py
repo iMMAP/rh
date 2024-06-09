bb40876e92cf49adbd854af8e6dfc740b227d5b2
Shahryar Tayeb <shahryar.dev@gmail.com>
2024-06-09 13:40:20 +0430

    add groups and permission fixture

--- src/users/signals.py
+++ src/users/signals.py
@@ -9,11 +9,14 @@ from .models import Profile
 def pre_save_profile(sender, instance, **kwargs):
     if instance.pk:
         old_instance = Profile.objects.get(pk=instance.pk)
-        if old_instance.organization != instance.organization:
-            org_lead_group = old_instance.user.groups.filter(name=f"{old_instance.organization.code.upper()}_ORG_LEADS")
-            if org_lead_group.exists():
-                # Remove user from ORG_LEAD group
-                instance.user.groups.remove(org_lead_group[0].id)
+        if old_instance and old_instance.organization != instance.organization:
+            if old_instance.organization:
+                org_lead_group = old_instance.user.groups.filter(
+                    name=f"{old_instance.organization.code.upper()}_ORG_LEADS"
+                )
+                if org_lead_group.exists():
+                    # Remove user from ORG_LEAD group
+                    instance.user.groups.remove(org_lead_group[0].id)
 
 
 @receiver(post_save, sender=User)

c90ea9dedf8863cba7220f028a6939a23619a886
Shahryar Tayeb <shahryar.dev@gmail.com>
2024-06-10 13:17:04 +0430

    added new fields to project filter

--- src/rh/filters.py
+++ src/rh/filters.py
@@ -1,13 +1,36 @@
 import django_filters
 from django import forms
+from django.contrib.auth.models import User
 
 from .models import ActivityDomain, Cluster, Donor, Organization, Project
+from django.db import models
+
+
+def clusters(request):
+    if request is None:
+        return Cluster.objects.none()
+
+    return request.user.profile.clusters.all()
+
+# def organization(request):
+#     if request is None:
+#         return Organization.objects.none()
+
+#     return Organization.objects.filter(clusters__in=request.user.profile.clusters.all())
 
 
 class ProjectsFilter(django_filters.FilterSet):
-    # FIXME: Fix the options issue
+    organization = django_filters.ModelMultipleChoiceFilter(
+        queryset=Organization.objects.all(), widget=forms.SelectMultiple(attrs={"class": "input-select"})
+    )
     clusters = django_filters.ModelMultipleChoiceFilter(
-        queryset=Cluster.objects.all(), widget=forms.SelectMultiple(attrs={"class": "input-select"})
+        queryset=clusters, widget=forms.SelectMultiple(attrs={"class": "input-select"})
+    )
+    activity_domains = django_filters.ModelMultipleChoiceFilter(
+        queryset=ActivityDomain.objects.all(), widget=forms.SelectMultiple(attrs={"class": "input-select"})
+    )
+    programme_partners = django_filters.ModelMultipleChoiceFilter(
+        queryset=Organization.objects.all(), widget=forms.SelectMultiple(attrs={"class": "input-select"})
     )
     implementing_partners = django_filters.ModelMultipleChoiceFilter(
         queryset=Organization.objects.all(), widget=forms.SelectMultiple(attrs={"class": "input-select"})
@@ -16,13 +39,35 @@ class ProjectsFilter(django_filters.FilterSet):
         queryset=Donor.objects.all(),
         widget=forms.SelectMultiple(attrs={"class": "input-select"}),
     )
-    programme_partners = django_filters.ModelMultipleChoiceFilter(
-        queryset=Organization.objects.all(), widget=forms.SelectMultiple(attrs={"class": "input-select"})
-    )
-    activity_domains = django_filters.ModelMultipleChoiceFilter(
-        queryset=ActivityDomain.objects.all(), widget=forms.SelectMultiple(attrs={"class": "input-select"})
+    user = django_filters.ModelMultipleChoiceFilter(
+        queryset=User.objects.all(),
+        widget=forms.SelectMultiple(attrs={"class": "input-select"}),
     )
 
     class Meta:
         model = Project
-        fields = "__all__"
+        fields = {
+            'title': ['contains'], 
+            'code': ['exact'], 
+            "start_date":["exact"],
+            "end_date":["exact"],
+            "is_hrp_project":["exact"],
+        }
+        filter_overrides = {
+            models.BooleanField: {
+                "filter_class": django_filters.BooleanFilter,
+                "extra": lambda f: {
+                    "widget": forms.CheckboxInput,
+                },
+            },
+            models.DateTimeField: {
+                "filter_class": django_filters.DateFilter,
+                "extra": lambda f: {
+                    "widget": forms.DateInput(
+                        attrs={
+                            "type": "date",
+                        }
+                    ),
+                },
+            },
+        }

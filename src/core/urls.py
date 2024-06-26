b9145ea606c4d298fdc5f5fcddd1a89d375122e4
Shahryar Tayeb <shahryar.dev@gmail.com>
2024-06-26 16:10:49 +0430

    fix debug-toolbar error in test mode

--- src/core/urls.py
+++ src/core/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
 
-admin.site.site_header = "ReportHub Admin"
+admin.site.site_header = f"{settings.APP_NAME} Admin"
 admin.site.site_title = settings.APP_NAME
 
 urlpatterns = [
@@ -16,8 +16,13 @@ urlpatterns = [
     path("chaining/", include("smart_selects.urls")),
     path("maintenance/", TemplateView.as_view(template_name="maintenance.html"), name="maintenance"),
     path("", include("django_vite_plugin.urls")),
-    path("__debug__/", include("debug_toolbar.urls")),
 ]
+
+if settings.DEBUG or not settings.TESTING:
+    urlpatterns = [
+        *urlpatterns,
+        path("__debug__/", include("debug_toolbar.urls")),
+    ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

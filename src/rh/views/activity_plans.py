b01d27625a52758f4241c01fc26f74c77e7d2184
Shahryar Tayeb <shahryar.dev@gmail.com>
2024-07-04 16:30:42 +0430

    activity_plans dependent dropdowns

--- src/rh/views/activity_plans.py
+++ src/rh/views/activity_plans.py
@@ -68,7 +68,9 @@ def create_activity_plan(request, project):
 
             messages.success(
                 request,
-                f'The Activity Plan "<a href="{reverse("activity-plans-update", args=[activity_plan.pk])}">{activity_plan}</a>" was added successfully.',
+                mark_safe(
+                    f'The Activity Plan "<a href="{reverse("activity-plans-update", args=[activity_plan.pk])}">{activity_plan}</a>" was added successfully.',
+                ),
             )
             if "_continue" in request.POST:
                 return redirect("activity-plans-update", pk=activity_plan.pk)
@@ -157,35 +159,33 @@ def list_activity_plans(request, project):
 @login_required
 def update_indicator_type(request):
     """Indicator related types fields"""
-    if request.method == "POST":
-        activity_plan_id = request.POST.get("activity_plan", "")
-        indicator_id = request.POST.get("id")
-        prefix = request.POST.get("prefix")
-        indicator_type_fields = [
-            "package_type",
-            "unit_type",
-            "grant_type",
-            "transfer_category",
-            "currency",
-            "transfer_mechanism_type",
-            "implement_modility_type",
-        ]
-        initial_data = {}
-        if activity_plan_id:
-            activity_plan = get_object_or_404(ActivityPlan, pk=activity_plan_id)
-            if (activity_plan.indicator) and (str(activity_plan.indicator.pk) != indicator_id):
-                fields_to_update = {field: None for field in indicator_type_fields}
+    activity_plan_id = request.GET.get("activity_plan", "")
+    indicator_id = request.GET.get("indicator")
 
-                # Update the fields in one query
-                ActivityPlan.objects.filter(pk=activity_plan.pk).update(**fields_to_update)
+    indicator_type_fields = [
+        "package_type",
+        "unit_type",
+        "grant_type",
+        "transfer_category",
+        "currency",
+        "transfer_mechanism_type",
+        "implement_modility_type",
+    ]
 
-            initial_data = {field: getattr(activity_plan, field, None) for field in indicator_type_fields}
+    initial_data = {}
+    if activity_plan_id:
+        activity_plan = get_object_or_404(ActivityPlan, pk=activity_plan_id)
+        if (activity_plan.indicator) and (str(activity_plan.indicator.pk) != indicator_id):
+            fields_to_update = {field: None for field in indicator_type_fields}
 
-        indicator = Indicator.objects.get(id=indicator_id)
-        indicator_form = ProjectIndicatorTypeForm(prefix=prefix, initial=initial_data)
-        context = {"indicator": indicator, "indicator_form": indicator_form}
+            # Update the fields in one query
+            ActivityPlan.objects.filter(pk=activity_plan.pk).update(**fields_to_update)
 
-        html = render_to_string("rh/projects/views/_indicator_types.html", context)
+        initial_data = {field: getattr(activity_plan, field, None) for field in indicator_type_fields}
 
-        # Return JSON response containing the generated HTML
-        return JsonResponse({"html": html})
+    indicator = Indicator.objects.get(id=indicator_id)
+    indicator_form = ProjectIndicatorTypeForm(initial=initial_data)
+    context = {"indicator": indicator, "indicator_form": indicator_form}
+
+    return render(request,"rh/projects/views/_indicator_types.html", context)
+

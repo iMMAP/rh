327c176b47468428aaba2c8c768ce55f1ad4fb41
Shahryar Tayeb <shahryar.dev@gmail.com>
2024-09-29 16:26:15 +0430

    montlhly reached and filter by date implemented

--- src/rh/views/organizations.py
+++ src/rh/views/organizations.py
@@ -1,9 +1,13 @@
+import datetime
+
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required, permission_required
 from django.core.cache import cache
 from django.core.exceptions import PermissionDenied
+from django.db.models import Count, Sum
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, render
+from project_reports.models import ProjectMonthlyReport
 from rh.models import Organization, TargetLocation
 from rh.utils import is_cluster_lead
 
@@ -11,6 +15,61 @@ from ..forms import (
     OrganizationForm,
     UpdateOrganizationForm,
 )
+
+
+@login_required
+def dashboard_5w(request, code):
+    org = get_object_or_404(Organization, code=code)
+
+    if not org.code == request.user.profile.organization.code and not is_cluster_lead(
+        request.user, org.clusters.values_list("code", flat=True)
+    ):
+        raise PermissionDenied
+
+    user_org = request.user.profile.organization
+
+    from_date = request.GET.get("from", datetime.date(datetime.datetime.now().year, 1, 1))
+    to_date = request.GET.get("to", datetime.datetime.now().date())
+
+    counts = ProjectMonthlyReport.objects.filter(
+        project__organization=user_org,
+        state__in=["submited", "completed"],
+        from_date__lte=to_date,
+        to_date__gte=from_date,
+    ).aggregate(
+        report_indicators_count=Count("activityplanreport__activity_plan__indicator", distinct=True),
+        report_implementing_partners_count=Count(
+            "activityplanreport__targetlocationreport__target_location__implementing_partner", distinct=True
+        ),
+        report_target_location_province_count=Count(
+            "activityplanreport__targetlocationreport__target_location__province", distinct=True
+        ),
+    )
+
+    people_reached_data = (
+        ProjectMonthlyReport.objects.filter(
+            project__organization=user_org,
+            state__in=["submited", "completed"],
+            from_date__lte=to_date,
+            to_date__gte=from_date,
+            activityplanreport__beneficiary_status="new_beneficiary",
+        )
+        .order_by("from_date")
+        .values("from_date")
+        .annotate(
+            total_people_reached=Sum("activityplanreport__targetlocationreport__disaggregationlocationreport__reached")
+        )
+    )
+
+    labels = [report["from_date"].strftime("%b") for report in people_reached_data]
+    data = [
+        report["total_people_reached"] if report["total_people_reached"] is not None else 0
+        for report in people_reached_data
+    ]
+
+    context = {"org": org, "counts": counts, "people_reached_labels": labels, "people_reached_data": data}
+
+    return render(request, "rh/5w_dashboard.html", context)
 
 
 @login_required

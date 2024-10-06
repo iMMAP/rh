0c3a4d6b75e3d9d63209f58d05756297b9a12afc
Esmatullah <esmatullah.hashimi2016@gmail.com>
2024-10-06 09:30:19 +0430

    import template customized

--- src/project_reports/views/views.py
+++ src/project_reports/views/views.py
@@ -1,11 +1,15 @@
 import csv
-
+import base64
+import datetime
+import json
+from io import BytesIO
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
 from django.views.decorators.http import require_http_methods
+from openpyxl import Workbook
 from rh.models import (
     ActivityDomain,
     ActivityPlan,
@@ -33,61 +37,28 @@ from ..models import (
     ResponseType,
     TargetLocationReport,
 )
-
+from ..utils import write_import_report_template_sheet
 RECORDS_PER_PAGE = 10
 
 
 @login_required
 def export_report_activities_import_template(request, report):
     monthly_report = get_object_or_404(ProjectMonthlyReport, pk=report)
-    columns = [
-        "project_code",
-        "indicator",
-        "activity_domain",
-        "activity_type",
-        "response_types",
-        "implementing_partners",
-        "package_type",
-        "unit_type",
-        "units",
-        "no_of_transfers",
-        "grant_type",
-        "transfer_category",
-        "currency",
-        "transfer_mechanism_type",
-        "implement_modility_type",
-        "beneficiary_status",
-        "admin0name",
-        "admin0pcode",
-        "admin1pcode",
-        "admin1name",
-        "admin2pcode",
-        "admin2name",
-        "zone",
-        "location_type",
-        "facility_site_type",
-        "facility_monitoring",
-        "facility_id",
-        "facility_name",
-        "facility_lat",
-        "facility_long",
-    ]
 
-    disaggregation_columns = list(
-        Disaggregation.objects.filter(clusters__in=monthly_report.project.clusters.all())
-        .distinct()
-        .values_list("name", flat=True)
-    )
+    workbook = Workbook()
+    write_import_report_template_sheet(workbook,monthly_report)
+    excel_file = BytesIO()
+    workbook.save(excel_file)
+    excel_file.seek(0)
 
-    all_columns = columns + disaggregation_columns
+    response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+    response['Content-Disposition'] = 'attachment; filename="activity_plans_import_template.xlsx"'
 
-    response = HttpResponse(content_type="text/csv")
-    response["Content-Disposition"] = "attachment; filename=activity_plans_import_template.csv"
-
-    writer = csv.writer(response)
-    writer.writerow(all_columns)
+    # Save the workbook to the response
+    workbook.save(response)
 
     return response
+  
 
 
 @login_required
